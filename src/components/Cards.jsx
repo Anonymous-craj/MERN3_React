f57fb5b48e0003a9b77a8fa85c1d5c8d6f3a597f
Anonymous-craj <sirajbasnet5@gmail.com>
2025-09-15 18:59:49 +0545

    integrated singleBlog and delete api in our frontend

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,48 +1,50 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Cards = (props) => {
   return (
     <>
-      <div className="bg-slate-800 text-white rounded-xl shadow-md w-80 flex flex-col">
-        <div className="w-full h-48 overflow-hidden rounded-t-xl">
-          <img
-            className="w-full h-full object-cover"
-            src={`http://localhost:3000/${props.blog.image}`}
-            alt=""
-          />
-        </div>
-        <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {props.blog.title}
-            </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            {props.blog.subtitle}
-          </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
+      <Link to={`/blog/${props.blog._id}`}>
+        <div className="bg-slate-800 text-white rounded-xl shadow-md w-80 flex flex-col">
+          <div className="w-full h-48 overflow-hidden rounded-t-xl">
+            <img
+              className="w-full h-full object-cover"
+              src={`http://localhost:3000/${props.blog.image}`}
+              alt=""
+            />
+          </div>
+          <div className="p-5">
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {props.blog.title}
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              {props.blog.subtitle}
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
+              Read more
+              <svg
+                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
---
+++ src/pages/SingleBlog.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+export const SingleBlog = () => {
+  const navigate = useNavigate();
+  const [blog, setBlog] = useState({});
+
+  const { id } = useParams();
+
+  const fetchSingleBlog = async () => {
+    const response = await axios.get("http://localhost:3000/blog/" + id);
+    setBlog(response.data.data);
+  };
+
+  useEffect(() => {
+    fetchSingleBlog();
+  }, []);
+
+  const deleteBlog = async () => {
+    const response = await axios.delete("http://localhost:3000/blog/" + id);
+    if (response.status === 200) {
+      alert("Blog deleted successfully");
+      navigate("/");
+    } else {
+      alert("Something went wrong!");
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-md mx-auto rounded-md overflow-hidden shadow-md hover:shadow-lg">
+        <div className="relative">
+          <img
+            className="w-full mt-[70px]"
+            src={`http://localhost:3000/${blog.image}`}
+            alt="Product Image"
+          />
+        </div>
+        <div className="p-4 text-center">
+          <h3 className="text-lg font-medium mb-2">{blog.title}</h3>
+          <span class="text-black">{blog.subtitle}</span>
+          <p className="text-gray-600 text-sm mt-1 mb-4">{blog.description}</p>
+          <div className="flex items-center justify-between">
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded cursor-pointer">
+              Edit
+            </button>
+            <button
+              onClick={deleteBlog}
+              className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded cursor-pointer"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
